fa2b170803be55990e8239e476560cd356022142
srikantKanekar <kanekarsrikant01@gmail.com>
2021-09-13 17:52:27 +0530

    added auth screens

--- app/src/main/java/com/example/myapplication/database/AppDatabase.kt
+++ app/src/main/java/com/example/myapplication/database/AppDatabase.kt
@@ -8,13 +8,13 @@ import com.example.myapplication.database.entity.Token
 import com.example.myapplication.database.entity.User
 
 @Database(entities = [Token::class, User::class], version = 1)
-abstract class AppDatabase: RoomDatabase() {
+abstract class AppDatabase : RoomDatabase() {
 
     abstract fun getTokenDao(): TokenDao
 
     abstract fun getUserDao(): UserDao
 
-    companion object{
+    companion object {
         const val DATABASE_NAME: String = "DATABASE_NAME"
     }
 }
